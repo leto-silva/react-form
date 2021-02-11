80a5e68f6e3079f35a03f718d354d9d308ad6668
Wellington Eugenio <wellington@Construtorajole.local>
2021-02-11 15:29:20 -0300

    finalizando Myform

--- src/Component/MyForm.js
+++ src/Component/MyForm.js
@@ -6,14 +6,28 @@ class MyForm extends Component{
        super(props);
 
        this.state = {
-           name: 'ABC'
+           name: 'ABC',
+           fruit: 'orange',
+           message: ''
        }
+
+       this.fruits = [
+           {name: 'apple' , value: 'Apple'},
+           {name: 'banana', value: 'Banana'},
+           {name: 'orange', value: 'Orange'},
+       ]
+
        this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event){
+
+        const target = event.target,
+            value = target.type === 'checkbox' ? target.checked : target.value,
+            name = target.name;
+
         this.setState({
-            name: event.target.value
+            [name]: value
         })
     }
 
@@ -23,11 +37,29 @@ class MyForm extends Component{
 
         return(
             <form>
-                <label>
-                    Name:
-                    <input type="text" name="name"  value={state.name} onChange={this.handleChange} />
-                </label>
-                <input type="submit" name="enviar" />
+                <div>
+                    <label>
+                        Name:
+                        <input type="text" name="name"  value={state.name} onChange={this.handleChange} />
+                    </label>
+                </div>
+                <div>  
+                    <label>
+                        Fruit:
+                        <select name="fruit" value={state.fruit} onChange={this.handleChange}>
+                            { 
+                                this.fruits.map(fruit => <option value={fruit.value}>{fruit.name}</option>)
+                            }
+                        </select>
+                    </label>
+                </div>
+                <div>    
+                    <label>
+                        Message:
+                        <textarea name="message" value={state.message} onChange={this.handleChange}/>
+                    </label>
+                    <input type="submit" name="enviar" />
+                </div>    
             </form>
         )
     }
